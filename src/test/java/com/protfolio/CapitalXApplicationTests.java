3cddd6c29b39e964e18f91176d4a21ec40f09f61
shrikant <shrikant2169@gmail.com>
2026-01-30 19:42:43 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/protfolio/CapitalXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
